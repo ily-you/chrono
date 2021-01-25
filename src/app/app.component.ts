fd1ef7fc3a5d7e0305042717a51f8b9638fde725
ilyasse <digne.ma@laposte.net>
2021-01-25 19:14:59 +0100

    started & paused

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chrono';
+  public isStarted = false;
+  public isPaused = false;
+  public time = 0;
+
+  public start(): void{
+    this.isStarted = true;
+  }
+
+  public pause(): void{
+    this.isPaused = true;
+  }
+
+  public continue(): void{
+    this.isPaused = false;
+  }
+
 }
