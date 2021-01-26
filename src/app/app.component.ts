539d42f95332a605a00269f2df1b9d80d0b4d775
ilyasse <digne.ma@laposte.net>
2021-01-26 14:18:45 +0100

    rajout des boutons et chrono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ReflectiveInjector } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +10,52 @@ export class AppComponent {
   public isStarted = false;
   public isPaused = false;
   public time = 0;
-
+ 
+  private idInterval: number|null = null;
+  public arrayTime : Array<number> = [];
+ 
+  public retenir(): void { 
+   this.arrayTime.push(this.time)
+  }
+  public removeItem(index: number): void {
+    if (index >= 0 && index < this.arrayTime.length) {
+      this.arrayTime.splice(index, 1);
+      console.log()
+    }
+  }
   public start(): void{
     this.isStarted = true;
+    this.isPaused= false;
+  this.startTimer();
+  }
+
+  public stop(): void{
+    this.isStarted = false;
+  this.stopTimer();
+  this.time = 0;
   }
 
   public pause(): void{
     this.isPaused = true;
+  this.stopTimer();
+  this.retenir();
+   
   }
-
+  
   public continue(): void{
     this.isPaused = false;
+  this.startTimer();
   }
 
+private startTimer(): void{
+  this.stopTimer();
+  this.idInterval = setInterval(() => {
+    this.time++;
+  }, 1);
+}
+private stopTimer(): void{
+  if(this.idInterval !== null){
+    clearInterval(this.idInterval);
+  }
+}
 }
